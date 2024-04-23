4cfc594bb56d6af1f417ff5f0cdecad7a77e71d8
Rhisav Ray <71272108+RhisavRay@users.noreply.github.com>
2024-04-23 10:09:52 +0530

    Test code

--- q9_string_rev/src/main.rs
+++ q9_string_rev/src/main.rs
@@ -1,3 +1,11 @@
+fn reverse_string(s: &str) -> String{
+    s.chars().rev().collect();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let original = "Hello";
+    let reversed = reverse_string(original);
+
+    println!("Original string was {}", original);
+    println!("Reversed string is {}", reversed);
 }
